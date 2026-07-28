f23b984d117cbdf681d2b06ff1fd6bd611e293a0
tanushk-10 <kandpaltanush@gmail.com>
2026-07-27 22:26:04 -0400

    more specific error message in feature map & function name fix

--- src/feature_map.py
+++ src/feature_map.py
@@ -1,8 +1,8 @@
 import numpy as np
 from qiskit import QuantumCircuit
 
-def applye(qc,x):
-# each encode block is super simple allw we need to do is two single-qubit gates Z-rotations + ZZ 
+def apply(qc,x):
+# each encode block is super simple all we need to do is two single-qubit gates Z-rotations + ZZ 
     qc.rz(-2 * x[0], 0)
     qc.rz(-2 * x[1], 1)
     phi_zz = (np.pi - x[0]) * (np.pi - x[1])
@@ -12,10 +12,11 @@ def applye(qc,x):
 
 def feature_map_circuit(x):
     if x.shape != (2,):
-        raise ValueError
+        raise ValueError("Feature map only supports 2 qubits/features") # since circuit is hard-coded to 2 qubits, x has to be 2 features
+
     qc = QuantumCircuit(2)
     qc.h(range(2))          # first Hadamard layer
-    applye(qc, x)  # first encode block
+    apply(qc, x)  # first encode block
     qc.h(range(2))          # second Hadamard layer
-    applye(qc, x)  # second encode block
+    apply(qc, x)  # second encode block
     return qc
