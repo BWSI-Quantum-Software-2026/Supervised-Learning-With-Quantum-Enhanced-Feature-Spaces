6bc94740de2d28fe72358463b250c17b8ad9059d
hfatima.xx <hayafatima5166@gmail.com>
2026-07-28 10:09:55 -0400

    Update optimizer.py

--- src/vqc/optimizer.py
+++ src/vqc/optimizer.py
@@ -12,14 +12,17 @@ gradient_estimate:any
 import numpy as np 
 import configparser #settings file (stores information)
 
-def spasa_step (theta, loss_fn, a:float = configparser.LEARNING_RATE, c:float = configparser.SPSA_PERTURABATION): #SPSA - it wil nudge every parameter randomly, check if it helped, then step in whichever direction looked better
+def spasa_step (theta, loss_fn, a:float = configparser.LEARNING_RATE, c:float = configparser.SPSA_PERTURABATION): 
+#SPSA - it wil nudge every parameter randomly, check if it helped, then step in whichever direction is better
 
-delta = np.random.choice([-1,1], size = len(theta)) #a coin flip for every parameter: +1 means try nudging it up and -1 meeans try nudging it down
+delta = np.random.choice([-1,1], size = len(theta)) 
+#a coin flip for every parameter (+1 means try nudging it up and -1 meeans try nudging it down)
 
-loss_plus = loss_fn(theta + c * delta) #move in the direction "delta" points and check the error
-loss_minus = loss_fn(theta - c * delta) #move in the OPPOSITE direction "delta" points and check the error
+loss_plus = loss_fn (theta + c * delta) #move in the direction "delta" points and check the error
+loss_minus = loss_fn (theta - c * delta) #move in the OPPOSITE direction "delta" points and check the error
 
-gradient_estimate = (loss_plus - loss_minus) / (2 * c) * delta #compare the two errors to guess whhoch way is downhill for each parameter at once
+gradient_estimate = (loss_plus - loss_minus) / (2 * c) * delta 
+#compares the two errors to guess which way is downhill for each parameter at once
 return theta - a * gradient_estimate #take a step down hill (learning rate "a")
 
 
