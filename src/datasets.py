71f97ade91dcf341dd60b3dfc3024173b7e8f75d
tanushk-10 <kandpaltanush@gmail.com>
2026-07-29 11:12:06 -0400

    adding iris dataset

--- src/datasets.py
+++ src/datasets.py
@@ -1,12 +1,13 @@
 import numpy as np
+from qiskit_machine_learning.datasets import ad_hoc_data
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 # shared by both teams teams - kernel and vqc pull data from here 
 
 
 def load_adhoc(training_size=20, test_size=5, gap=0.3, seed=12345): # training_size is PER CLASS so 20 gives us 40 points total
     # the paper's actual dataset. its made in a way that our feature map can easily
     # separate them and it will be accurate
-    from qiskit_machine_learning.datasets import ad_hoc_data
-
     # qiskit has its own global seed and so we set it so we get the same points every run
     try:
         from qiskit_machine_learning.utils import algorithm_globals
@@ -29,5 +30,17 @@ def load_adhoc(training_size=20, test_size=5, gap=0.3, seed=12345): # training_s
 
     return train_X, train_y, test_X, test_y # X is the points, y is the -1/+1 labels
 
+def load_iris_2feature(seed=0): # seed just has to be some fixed val 
+    # data that is not designed around our feature map whatsoever -- kind of like a reality check
+    # main test is to check if  the method does anything on data that wasn't meant for it
+    data = load_iris()
 
+    # Keep only 2 of the 4 features (petal length & width) b/c of our 2 circuit qubit and 2 of the 3 species.
+    X = data.data[:, [2, 3]]
+    y = data.target
+    keep = y < 2
+    X = X[keep]
+    y = y[keep]
+
+   
 
