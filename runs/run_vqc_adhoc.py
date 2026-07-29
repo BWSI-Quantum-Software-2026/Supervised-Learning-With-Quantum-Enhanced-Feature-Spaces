003f8bb266105576807e9272f3e0998cd49beeff
tanushk-10 <kandpaltanush@gmail.com>
2026-07-29 16:21:02 -0400

    Use cobyla and 2 ansatz layers for more accuracy with vqc for adhoc

--- runs/run_vqc_adhoc.py
+++ runs/run_vqc_adhoc.py
@@ -6,8 +6,10 @@ from src.vqc.model import VQCModel
 
 def main(shots=4096, seed=1):
     train_X, train_y, test_X, test_y = load_adhoc(training_size=15, test_size=5)
-    qvm = VQCModel(qubits=2, repetitions=1)
-    qvm.fit(train_X, train_y)
+    # 2 ansatz layers -- 1 layer isnt expressive enough to separate this data
+    qvm = VQCModel(qubits=2, repetitions=2)
+    # cobyla converges way better than spsa here
+    qvm.fit(train_X, train_y, method="cobyla", max_iters=200)
     accuracy = qvm.score(test_X, test_y)
     print(f"Test accuracy: {accuracy:.4f}")
 
