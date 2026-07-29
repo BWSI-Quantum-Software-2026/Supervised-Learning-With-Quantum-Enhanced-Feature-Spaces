aca937ff85255fb3ea72e50b338bb0903a165bfc
tanushk-10 <kandpaltanush@gmail.com>
2026-07-29 16:22:51 -0400

    small fix

--- runs/run_vqc_adhoc.py
+++ runs/run_vqc_adhoc.py
@@ -6,7 +6,7 @@ from src.vqc.model import VQCModel
 
 def main(shots=4096, seed=1):
     train_X, train_y, test_X, test_y = load_adhoc(training_size=15, test_size=5)
-    # 2 ansatz layers -- 1 layer isnt expressive enough to separate this data
+    # 2 layers, 1 wasnt enough to separate the data
     qvm = VQCModel(qubits=2, repetitions=2)
     # cobyla converges way better than spsa here
     qvm.fit(train_X, train_y, method="cobyla", max_iters=200)
